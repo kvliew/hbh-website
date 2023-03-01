d191b7d7fb22332798e527acea95a73c4cfc7337
Khin Liew <khinl@student.unimelb.edu.au>
2023-03-02 01:01:36 +1100

    added favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy
-  ("./src/styles.css")
+  eleventyConfig.addPassthroughCopy("./src/styles.css")
+  eleventyConfig.addPassthroughCopy("./src/assets/logo.svg")
   return {
     dir: {
       input: "src",
