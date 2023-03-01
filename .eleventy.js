bb318c60972e63e783b5d922e508d9037d437968
Khin Liew <khinl@student.unimelb.edu.au>
2023-03-01 17:13:59 +1100

    added landing page elements

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy
+  ("./src/styles.css")
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    }
+  }
+}
