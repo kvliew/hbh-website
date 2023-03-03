9e79794e726f8b323a3dacb8dc44f2fac330991b
Khin Liew <khinl@student.unimelb.edu.au>
2023-03-03 20:14:44 +1100

    created separate css files for all pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src//styles/main-styles.css");
   eleventyConfig.addPassthroughCopy("./src/styles/host-page.css");
+  eleventyConfig.addPassthroughCopy("./src/styles/mission-page.css");
+  eleventyConfig.addPassthroughCopy("./src/styles/episodes-page.css");
+  eleventyConfig.addPassthroughCopy("./src/styles/contact-page.css");
   eleventyConfig.addPassthroughCopy("./src/assets/logo.svg");
   eleventyConfig.addPassthroughCopy("./src/script.js");
   eleventyConfig.addPassthroughCopy("./src/assets/colin-bischof.jpg");
