520b2a58eb6e3573fc8a2b2ee5b62f6ac053e9ac
Khin Liew <khinl@student.unimelb.edu.au>
2023-03-03 19:49:47 +1100

    file structure update
    - css files placed in enclosing folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/styles.css");
+  eleventyConfig.addPassthroughCopy("./src//styles/main-styles.css");
+  eleventyConfig.addPassthroughCopy("./src/styles/host-page.css");
   eleventyConfig.addPassthroughCopy("./src/assets/logo.svg");
   eleventyConfig.addPassthroughCopy("./src/script.js");
   eleventyConfig.addPassthroughCopy("./src/assets/colin-bischof.jpg");
   eleventyConfig.addPassthroughCopy("./src/assets/darren-brennan.jpg");
-  eleventyConfig.addPassthroughCopy("./src/host-page.css");
   return {
     dir: {
       input: "src",
