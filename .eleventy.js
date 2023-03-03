3746417c56e16eb666df0e4c987b1315faa3c1e6
Khin Liew <khinl@student.unimelb.edu.au>
2023-03-03 19:33:02 +1100

    added hosts page

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles.css");
   eleventyConfig.addPassthroughCopy("./src/assets/logo.svg");
   eleventyConfig.addPassthroughCopy("./src/script.js");
+  eleventyConfig.addPassthroughCopy("./src/assets/colin-bischof.jpg");
+  eleventyConfig.addPassthroughCopy("./src/assets/darren-brennan.jpg");
+  eleventyConfig.addPassthroughCopy("./src/host-page.css");
   return {
     dir: {
       input: "src",
