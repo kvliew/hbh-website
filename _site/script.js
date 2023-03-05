cfa2b77bcfa2a82c9b6e556476c3d9686d80da4f
Khin Liew <khinl@student.unimelb.edu.au>
2023-03-05 12:33:49 +1100

    activated watch-now button
    added anchor tag for 'see more' link

--- _site/script.js
+++ _site/script.js
@@ -1,5 +1,12 @@
-var menuOverlay = document.getElementById("overlay-wrapper");
+const menuOverlay = document.getElementById("overlay-wrapper");
 
 function handleMenu() {
   menuOverlay.classList.toggle("active");
 }
+
+const watchNowButton = document.getElementById("watch-now");
+const secondHomeSection = document.getElementById("home-section-2");
+
+watchNowButton.addEventListener("click", () => {
+  secondHomeSection.scrollIntoView();
+});
