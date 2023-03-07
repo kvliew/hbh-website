95bcb00d979b8b0f8bebd0c3d7bd9ccbe85510ae
Khin Liew <khinl@student.unimelb.edu.au>
2023-03-07 19:07:45 +1100

    completed posts pipeline
    Individual .MD posts now contain their own URL, connected to stylesheet

--- .eleventy.js
+++ .eleventy.js
@@ -4,10 +4,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles/mission-page.css");
   eleventyConfig.addPassthroughCopy("./src/styles/episodes-page.css");
   eleventyConfig.addPassthroughCopy("./src/styles/contact-page.css");
+  eleventyConfig.addPassthroughCopy("./src/styles/post.css");
   eleventyConfig.addPassthroughCopy("./src/assets/logo.svg");
   eleventyConfig.addPassthroughCopy("./src/script.js");
   eleventyConfig.addPassthroughCopy("./src/assets/colin-bischof.jpg");
   eleventyConfig.addPassthroughCopy("./src/assets/darren-brennan.jpg");
+  eleventyConfig.addCollection("posts");
   return {
     dir: {
       input: "src",
--- src/posts/ep-1.md
+++ src/posts/ep-1.md
@@ -1,6 +1,7 @@
 ---
 title: Introduction to the Home Building Hub Podcast
 number: 1
+layout: post.njk
 ---
 
 First post content
