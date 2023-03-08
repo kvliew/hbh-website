c3977e11b733e09d473a6b648f00a32226288592
Khin Liew <khinl@student.unimelb.edu.au>
2023-03-08 16:24:14 +1100

    added transition effects
    to about paragraph and recent episode cards on home page

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src//styles/");
   eleventyConfig.addPassthroughCopy("./src/assets/logo.svg");
-  eleventyConfig.addPassthroughCopy("./src/script.js");
+  eleventyConfig.addPassthroughCopy("./src/watch-now-script.js");
   eleventyConfig.addPassthroughCopy("./src/mobile-menu-script.js");
   eleventyConfig.addPassthroughCopy("./src/assets/colin-bischof.jpg");
   eleventyConfig.addPassthroughCopy("./src/assets/darren-brennan.jpg");
