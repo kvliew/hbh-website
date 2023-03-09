03bc5da7cc639f0d7a83417a8d17725dd81d183c
Khin Liew <khinl@student.unimelb.edu.au>
2023-03-09 16:11:58 +1100

    added fade-in animation to episode cards

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets/logo.svg");
   eleventyConfig.addPassthroughCopy("./src/watch-now-script.js");
   eleventyConfig.addPassthroughCopy("./src/mobile-menu-script.js");
+  eleventyConfig.addPassthroughCopy("./src/transition-script.js");
   eleventyConfig.addPassthroughCopy("./src/assets/colin-bischof.jpg");
   eleventyConfig.addPassthroughCopy("./src/assets/darren-brennan.jpg");
   eleventyConfig.addCollection("posts");
--- src/watch-now-script.js
+++ src/watch-now-script.js
@@ -4,15 +4,3 @@ const secondHomeSection = document.getElementById("home-section-2");
 watchNowButton.addEventListener("click", () => {
   secondHomeSection.scrollIntoView();
 });
-
-// scroll transition script
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if(entry.isIntersecting) {
-      entry.target.classList.add('show');
-    }
-  });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
