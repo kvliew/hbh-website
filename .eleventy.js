bb33e2668af32cfb7cd4f87205e555b22bf4af9c
Khin Liew <khinl@student.unimelb.edu.au>
2023-03-22 22:32:13 +1100

    added posts collections filter and limited recent episodes section to 3 eps

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/watch-now-script.js");
   eleventyConfig.addPassthroughCopy("./src/mobile-menu-script.js");
   eleventyConfig.addPassthroughCopy("./src/transition-script.js");
-  eleventyConfig.addPassthroughCopy("./src/assets/colin-bischof.jpg");
-  eleventyConfig.addPassthroughCopy("./src/assets/darren-brennan.jpg");
+  // eleventyConfig.addCollection("posts", function(collection) {
+  //   return collection.getFilteredByGlob("./src/posts/*.md").reverse();
+  // });
   eleventyConfig.addCollection("posts");
   return {
     dir: {
