cb382b712affe1078ed385e8bf8ca575d4a55ad0
Khin Liew <khinl@student.unimelb.edu.au>
2023-04-19 16:19:41 +1000

    created error page
    - added "Home Building Hub" to titles of each page
    - sort collections by episode number

--- .eleventy.js
+++ .eleventy.js
@@ -4,10 +4,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/watch-now-script.js");
   eleventyConfig.addPassthroughCopy("./src/mobile-menu-script.js");
   eleventyConfig.addPassthroughCopy("./src/transition-script.js");
-  // eleventyConfig.addCollection("posts", function(collection) {
-  //   return collection.getFilteredByGlob("./src/posts/*.md").reverse();
-  // });
-  eleventyConfig.addCollection("posts");
+  eleventyConfig.addCollection("posts", function(collection) {
+    return collection.getFilteredByGlob("./src/posts/*.md")
+      .sort((a, b) => a.data.number - b.data.number);
+  });
+  // eleventyConfig.addCollection("posts");
   return {
     dir: {
       input: "src",
