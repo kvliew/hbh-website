10d00a3afcfdacda26432d8ef45670ef7b670474
Khin Liew <khinl@student.unimelb.edu.au>
2025-01-23 11:24:47 +1100

    articles skeletal

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/watch-now-script.js");
   eleventyConfig.addPassthroughCopy("./src/mobile-menu-script.js");
   eleventyConfig.addPassthroughCopy("./src/transition-script.js");
+  eleventyConfig.addCollection("articles", function(collection) {
+    return collection.getFilteredByGlob("./src/articles/*.md")
+      .sort((a, b) => a.data.articlenumber - b.data.articlenumber);
+  });
   eleventyConfig.addCollection("guests", function(collection) {
     return collection.getFilteredByGlob("./src/guests/*.md")
       .sort((a, b) => a.data.guestepisodenumber - b.data.guestepisodenumber);
