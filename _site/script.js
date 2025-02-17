9463fb66374c18ddbd4bb2c273ae27845784b932
Khin Liew <khinvyn@gmail.com>
2025-02-17 11:05:50 +1100

    Update gitignore to keep admin panel

--- _site/script.js
+++
@@ -1,6 +0,0 @@
-const watchNowButton = document.getElementById("watch-now");
-const secondHomeSection = document.getElementById("home-section-2");
-
-watchNowButton.addEventListener("click", () => {
-  secondHomeSection.scrollIntoView();
-});
