0fa8d1bf84eceacb722867939d60d13e2a65dd5b
Khin Liew <khinvyn@gmail.com>
2025-02-19 00:44:38 +1100

    floated navbar

--- src/scripts/web-navbar.js
+++ src/scripts/web-navbar.js
@@ -1,6 +1,31 @@
-const navBar = document.getElementById("navBar");
+const navBar = document.getElementById("nav-bar");
+const logo = document.getElementById("logo");
 const dropdown = document.getElementById("resources-dropdown");
+const resourcesLabel = document.getElementById("resources-text");
+// const arrow = document.getElementById("dropdown-arrow");
 
 dropdown.addEventListener('click', function() {
   dropdown.classList.toggle('show');
+  resourcesLabel.classList.toggle('show');
+  // arrow.classList.toggle('show');
+});
+
+document.addEventListener('click', (e) => {
+  if (!dropdown.contains(e.target)) { 
+    dropdown.classList.remove('show');
+    resourcesLabel.classList.remove('show');
+    // arrow.classList.remove('show');
+  }
+});
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 50) {
+    navBar.classList.add('scrolled');
+    logo.classList.add('scrolled');
+    // navBar.classList.add('sticky-nav');
+  } else {
+    navBar.classList.remove('scrolled');
+    logo.classList.remove('scrolled');
+    // document.body.classList.remove('sticky-nav');
+  }
 });
